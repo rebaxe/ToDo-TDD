f50c2377c307b45c4949b48edbfa36d7bfa137f5
Rebecca Axelsson <ra223ai@student.lnu.se>
2021-11-11 17:33:11 +0100

    REFACTOR: Add more test cases

--- src/tests/TaskList.test.ts
+++ src/tests/TaskList.test.ts
@@ -1,10 +1,25 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable max-classes-per-file */
 import Task from '../classes/Task'
 import TaskList from '../classes/TaskList'
 
 class StubTask extends Task {
-  // eslint-disable-next-line class-methods-use-this
   getDescription() {
     return 'Water plants'
+  }
+
+  getStatus() {
+    return false
+  }
+}
+
+class StubTaskTrue extends Task {
+  getDescription() {
+    return 'Water plants'
+  }
+
+  getStatus() {
+    return true
   }
 }
 
@@ -28,5 +43,28 @@ describe('Task list tests', () => {
     tasklist.delete(task)
     const list = tasklist.getTasks()
     expect(list.length).toEqual(0)
+  })
+  test('TC2d. Add tasks to list', () => {
+    const tasklist = new TaskList()
+    const taskOne = new StubTaskTrue('Water plants', true)
+    tasklist.add(taskOne)
+    const taskTwo = new StubTask('Water plants', false)
+    tasklist.add(taskTwo)
+    const taskThree = new StubTaskTrue('Water plants', true)
+    tasklist.add(taskThree)
+    const list = tasklist.getTasks()
+    expect(list.length).toEqual(3)
+  })
+  test('TC2e. Delete task from list with three tasks', () => {
+    const tasklist = new TaskList()
+    const taskOne = new StubTaskTrue('Water plants', true)
+    tasklist.add(taskOne)
+    const taskTwo = new StubTask('Water plants', false)
+    tasklist.add(taskTwo)
+    const taskThree = new StubTaskTrue('Water plants', true)
+    tasklist.add(taskThree)
+    tasklist.delete(taskTwo)
+    const list = tasklist.getTasks()
+    expect(list[1].getStatus()).toBeTruthy()
   })
 })
