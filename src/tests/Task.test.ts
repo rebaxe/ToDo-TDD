62fe03ef8bb530415f07c55f4a1372943978a9c1
Rebecca Axelsson <ra223ai@student.lnu.se>
2021-11-11 16:52:02 +0100

    Change test case numeration

--- src/tests/Task.test.ts
+++ src/tests/Task.test.ts
@@ -1,17 +1,17 @@
 import Task from '../classes/Task'
 
 describe('Task tests', () => {
-  test('TC1. Create new task and get description', () => {
+  test('TC1a. Create new task and get description', () => {
     const task = new Task('Water plants', false)
     const description = task.getDescription()
     expect(description).toBe('Water plants')
   })
-  test('TC2. Create new task and get status', () => {
+  test('TC1b. Create new task and get status', () => {
     const task = new Task('Water plants', false)
     const status = task.getStatus()
     expect(status).toBe(false)
   })
-  test('TC3. Update task status', () => {
+  test('TC1c. Update task status', () => {
     const task = new Task('Water plants', false)
     task.setStatus(true)
     const status = task.getStatus()
