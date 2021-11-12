623220a3be7bd9103dbbf14219cdbabf2b333c80
Rebecca Axelsson <ra223ai@student.lnu.se>
2021-11-12 11:21:48 +0100

    RED: Task list - get points

--- src/tests/TaskList.test.ts
+++ src/tests/TaskList.test.ts
@@ -1,5 +1,7 @@
 /* eslint-disable class-methods-use-this */
 /* eslint-disable max-classes-per-file */
+import EasyPoint from '../classes/EasyPoint'
+import Point from '../classes/Point'
 import Task from '../classes/Task'
 import TaskList from '../classes/TaskList'
 
@@ -23,6 +25,18 @@ class StubTaskTrue extends Task {
   }
 }
 
+class StubPoint extends EasyPoint {}
+
+const stubPoint = new StubPoint()
+
+function comparePoints(point: any) {
+  return point as Point
+}
+
+function isPoints(points: any) {
+  return points.some((point: any) => comparePoints(point))
+}
+
 describe('Task list tests', () => {
   test('TC2a. Get task list', () => {
     const tasklist = new TaskList()
@@ -31,14 +45,14 @@ describe('Task list tests', () => {
   })
   test('TC2b. Add task to list', () => {
     const tasklist = new TaskList()
-    const task = new StubTask('Water plants', false)
+    const task = new StubTask('Water plants', false, stubPoint)
     tasklist.add(task)
     const list = tasklist.getTasks()
     expect(list[0].getDescription()).toBe('Water plants')
   })
   test('TC2c. Delete task from list', () => {
     const tasklist = new TaskList()
-    const task = new StubTask('Water plants', false)
+    const task = new StubTask('Water plants', false, stubPoint)
     tasklist.add(task)
     tasklist.delete(task)
     const list = tasklist.getTasks()
@@ -46,25 +60,36 @@ describe('Task list tests', () => {
   })
   test('TC2d. Add tasks to list', () => {
     const tasklist = new TaskList()
-    const taskOne = new StubTaskTrue('Water plants', true)
+    const taskOne = new StubTaskTrue('Water plants', true, stubPoint)
     tasklist.add(taskOne)
-    const taskTwo = new StubTask('Water plants', false)
+    const taskTwo = new StubTask('Water plants', false, stubPoint)
     tasklist.add(taskTwo)
-    const taskThree = new StubTaskTrue('Water plants', true)
+    const taskThree = new StubTaskTrue('Water plants', true, stubPoint)
     tasklist.add(taskThree)
     const list = tasklist.getTasks()
     expect(list.length).toEqual(3)
   })
   test('TC2e. Delete task from list with three tasks', () => {
     const tasklist = new TaskList()
-    const taskOne = new StubTaskTrue('Water plants', true)
+    const taskOne = new StubTaskTrue('Water plants', true, stubPoint)
     tasklist.add(taskOne)
-    const taskTwo = new StubTask('Water plants', false)
+    const taskTwo = new StubTask('Water plants', false, stubPoint)
     tasklist.add(taskTwo)
-    const taskThree = new StubTaskTrue('Water plants', true)
+    const taskThree = new StubTaskTrue('Water plants', true, stubPoint)
     tasklist.add(taskThree)
     tasklist.delete(taskTwo)
     const list = tasklist.getTasks()
     expect(list[1].getStatus()).toBeTruthy()
+  })
+  test('TC2f. Get task points', () => {
+    const tasklist = new TaskList()
+    const taskOne = new StubTaskTrue('Water plants', true, stubPoint)
+    tasklist.add(taskOne)
+    const taskTwo = new StubTask('Water plants', false, stubPoint)
+    tasklist.add(taskTwo)
+    const taskThree = new StubTaskTrue('Water plants', true, stubPoint)
+    tasklist.add(taskThree)
+    const points = tasklist.getPoints()
+    expect(isPoints(points)).toBeTruthy()
   })
 })
