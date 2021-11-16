6ba3c8cedfd7cc37cd3cf5419adbf7bda1808a9d
Kaj Berg <kb223aw@student.lnu.se>
2021-11-16 17:14:36 +0100

    working on mocking

--- src/tests/ToDo.test.ts
+++ src/tests/ToDo.test.ts
@@ -5,61 +5,65 @@ import UnfinishedTaskList from '../classes/UnfinishedTaskList'
 import ToDo from '../classes/ToDo'
 import Task from '../classes/Task'
 import EasyPoint from '../classes/EasyPoint'
-import TaskList from '../classes/TaskList'
 
-jest.unmock('../classes/ToDo')
-
-jest.mock('../classes/UnfinishedTaskList', () => {
-  return jest.fn().mockImplementation(() => {
-    return {
-      add: jest.fn(),
-      delete: jest.fn()
-    }
+beforeEach(() => {
+  jest.mock('../classes/UnfinishedTaskList', () => {
+    return jest.fn().mockImplementation(() => {
+      return {
+        add: jest.fn(),
+        delete: jest.fn()
+      }
+    })
   })
-})
-
-jest.mock('../classes/FinishedTaskList', () => {
-  return jest.fn().mockImplementation(() => {
-    return {
-      add: jest.fn(),
-      delete: jest.fn()
-    }
+  jest.mock('../classes/FinishedTaskList', () => {
+    return jest.fn().mockImplementation(() => {
+      return {
+        add: jest.fn(),
+        delete: jest.fn()
+      }
+    })
   })
-})
-
-jest.mock('../classes/Task', () => {
-  return jest.fn(() => {
-    return {
-      getStatus: true,
-      setStatus: jest.fn()
-    }
+  jest.mock('../classes/Task', () => {
+    return jest.fn(() => {
+      return {
+        getStatus: jest.fn(),
+        setStatus: jest.fn()
+      }
+    })
   })
+  jest.mock('../classes/EasyPoint.ts')
 })
-
-jest.mock('../classes/EasyPoint.ts')
 
 describe('ToDo tests', () => {
-  // test('TC6a. Get list of unfinished tasks', () => {
-  //   const unfinishedTaskList = todo.getUnFinishedTasks()
-  //   expect(unfinishedTaskList instanceof UnfinishedTaskList).toBeTruthy()
-  // })
-  // test('TC6b. Get list of finished tasks', () => {
-  //   const finishedTaskList = todo.getFinishedTasks()
-  //   expect(finishedTaskList instanceof FinishedTaskList).toBeTruthy()
-  // })
-  // test('TC6c. Get instance of point counter', () => {
-  //   const pointCounter = todo.getPointCounter()
-  //   expect(pointCounter instanceof PointCounter).toBeTruthy()
-  // })
-  test('TC6d. Mark task as finished', () => {
+  test('TC6a. Mark task as finished', () => {
     const unfinishedTaskList = new UnfinishedTaskList()
     const finishedTaskList = new FinishedTaskList()
-    // const toDo = new ToDo(new UnfinishedTaskList(), new FinishedTaskList())
     const toDo = new ToDo(unfinishedTaskList, finishedTaskList)
-    const task = new Task('test', true, new EasyPoint())
-    // const mockedTask = Task as jest.Mocked<typeof Task>
-    // mockedTask.getStatus.mockReturnValue(true)
-    // toDo.toggleStatus(mockedTask)
-    expect(toDo.toggleStatus).toHaveBeenCalled()
+
+    const task = new Task('test task', false, new EasyPoint())
+
+    const spyAdd = jest.spyOn(finishedTaskList, 'add')
+    const spyDelete = jest.spyOn(unfinishedTaskList, 'delete')
+
+    toDo.toggleStatus(task)
+    expect(spyAdd).toHaveBeenCalledWith(task)
+    expect(spyDelete).toHaveBeenCalledWith(task)
+    expect(spyAdd).toHaveBeenCalledTimes(1)
+    expect(spyDelete).toHaveBeenCalledTimes(1)
+  })
+  test('TC6b. Mark task as unfinished', () => {
+    const unfinishedTaskList = new UnfinishedTaskList()
+    const finishedTaskList = new FinishedTaskList()
+    const toDo = new ToDo(unfinishedTaskList, finishedTaskList)
+
+    const task = new Task('test task', true, new EasyPoint())
+    const spyAdd = jest.spyOn(unfinishedTaskList, 'add')
+    const spyDelete = jest.spyOn(finishedTaskList, 'delete')
+
+    toDo.toggleStatus(task)
+    expect(spyAdd).toHaveBeenCalledWith(task)
+    expect(spyDelete).toHaveBeenCalledWith(task)
+    expect(spyAdd).toHaveBeenCalledTimes(1)
+    expect(spyDelete).toHaveBeenCalledTimes(1)
   })
 })
