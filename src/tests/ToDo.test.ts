b6b1d1a9c96435a8d00c1f0f9cfa7f63cd71a114
Kaj Berg <kb223aw@student.lnu.se>
2021-11-17 11:07:03 +0100

    Refactor test

--- src/tests/ToDo.test.ts
+++ src/tests/ToDo.test.ts
@@ -6,42 +6,40 @@ import ToDo from '../classes/ToDo'
 import Task from '../classes/Task'
 import EasyPoint from '../classes/EasyPoint'
 
+jest.mock('../classes/Task')
+jest.mock('../classes/UnfinishedTaskList')
+jest.mock('../classes/FinishedTaskList')
+jest.mock('../classes/EasyPoint.ts')
+
+const unfinishedTaskList = new UnfinishedTaskList()
+const finishedTaskList = new FinishedTaskList()
+const toDo = new ToDo(unfinishedTaskList, finishedTaskList)
+const task = new Task('test task', false, new EasyPoint())
+
 beforeEach(() => {
   jest.clearAllMocks()
-  jest.mock('../classes/Task')
-  jest.mock('../classes/UnfinishedTaskList')
-  jest.mock('../classes/FinishedTaskList')
-  jest.mock('../classes/EasyPoint.ts')
 })
 
 describe('ToDo tests', () => {
-  test('TC6a. Mark task as finished', () => {
-    const unfinishedTaskList = new UnfinishedTaskList()
-    const finishedTaskList = new FinishedTaskList()
-    const toDo = new ToDo(unfinishedTaskList, finishedTaskList)
-
-    const task = new Task('test task', false, new EasyPoint())
+  it('TC6a. Mark task as finished', () => {
     jest.spyOn(task, 'getStatus').mockImplementation(() => false)
     const spyAdd = jest.spyOn(finishedTaskList, 'add')
     const spyDelete = jest.spyOn(unfinishedTaskList, 'delete')
 
     toDo.toggleStatus(task)
+
     expect(spyAdd).toHaveBeenCalledWith(task)
     expect(spyDelete).toHaveBeenCalledWith(task)
     expect(spyAdd).toHaveBeenCalledTimes(1)
     expect(spyDelete).toHaveBeenCalledTimes(1)
   })
-  test('TC6b. Mark task as unfinished', () => {
-    const unfinishedTaskList = new UnfinishedTaskList()
-    const finishedTaskList = new FinishedTaskList()
-    const toDo = new ToDo(unfinishedTaskList, finishedTaskList)
-
-    const task = new Task('test task', true, new EasyPoint())
+  it('TC6b. Mark task as unfinished', () => {
     jest.spyOn(task, 'getStatus').mockImplementation(() => true)
     const spyAdd = jest.spyOn(unfinishedTaskList, 'add')
     const spyDelete = jest.spyOn(finishedTaskList, 'delete')
 
     toDo.toggleStatus(task)
+
     expect(spyAdd).toHaveBeenCalledWith(task)
     expect(spyDelete).toHaveBeenCalledWith(task)
     expect(spyAdd).toHaveBeenCalledTimes(1)
