09d1ced1ce31073658ee418a55fbd4c5f2dce6dd
Rebecca Axelsson <ra223ai@student.lnu.se>
2021-11-30 11:28:41 +0100

    RED: Render ToDo-component

---
+++ src/tests/components/ToDo.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
+import ToDo from '../../components/ToDo'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('To Do component', () => {
+  it('Should render the to do component', () => {
+    const todo = shallow(<ToDo />)
+    expect(todo).toMatchSnapshot()
+  })
+})
