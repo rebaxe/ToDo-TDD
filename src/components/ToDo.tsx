e6a8c17d44992f276541781d705296e7cabd486e
Kaj Berg <kb223aw@student.lnu.se>
2021-11-30 13:28:28 +0100

    GREEN: Render To Do component

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -10,6 +10,10 @@ function ToDo(): JSX.Element {
   return (
     <div>
       <h1>Fun To Do</h1>
+      <form onSubmit={e => addTask(e)}>
+        <input name="todo" onChange={e => setNewTask(e.target.value)} value={newTask} type="text" />
+        <button type="submit">Add</button>
+      </form>
     </div>
   )
 }
--- src/tests/components/ToDo.test.ts
+++
@@ -1,9 +0,0 @@
-import { shallow } from 'enzyme'
-import ToDo from '../../components/ToDo'
-
-describe('To Do component', () => {
-  it('Should render the to do component', () => {
-    const todo = shallow(<ToDo />)
-    expect(todo).toMatchSnapshot()
-  })
-})
