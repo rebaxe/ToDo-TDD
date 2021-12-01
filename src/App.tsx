2922df37a0408164ba71de9119b61944b7d585b4
Rebecca Axelsson <ra223ai@student.lnu.se>
2021-12-01 12:17:42 +0100

    Style fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './global.css'
 import ToDo from './components/ToDo'
+import TodoFactory from './classes/ToDoFactory'
 
 function App(): JSX.Element {
+  const todoFactory = new TodoFactory()
+
   return (
     <>
-      <ToDo />
+      <ToDo todo={todoFactory.getToDo()} />
     </>
   )
 }
