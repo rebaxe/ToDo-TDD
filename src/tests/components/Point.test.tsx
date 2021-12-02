186edb4eac56e01b571748df0261abcbf3c63521
Rebecca Axelsson <ra223ai@student.lnu.se>
2021-12-02 10:56:49 +0100

    RED: Medium point

--- src/tests/components/Point.test.tsx
+++ src/tests/components/Point.test.tsx
@@ -3,10 +3,13 @@ import React from 'react'
 import { mount } from 'enzyme'
 import EasyPoint from '../../classes/EasyPoint'
 import Point from '../../components/Point'
+import MediumPoint from '../../classes/MediumPoint'
 
 jest.mock('../../classes/EasyPoint')
+jest.mock('../../classes/MediumPoint')
 
 const mockEasyPoint = new EasyPoint()
+const mockMediumPoint = new MediumPoint()
 
 let point: any
 
@@ -18,6 +21,11 @@ describe('Point component', () => {
   it('Should be an easy task', () => {
     point = mount(<Point point={mockEasyPoint} />)
     const easyPoint = point.find('.easyPoint')
+    expect(easyPoint).toHaveLength(1)
+  })
+  it('Should be a medium task', () => {
+    point = mount(<Point point={mockMediumPoint} />)
+    const easyPoint = point.find('.mediumPoint')
     expect(easyPoint).toHaveLength(1)
   })
 })
