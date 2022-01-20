ba3dbfb16e2bfe26a16ec4f13cad7be67ccc13f6
Rebecca Axelsson <ra223ai@student.lnu.se>
2022-01-20 13:13:23 +0100

    ✨ Add modal open when all tasks finished

--- src/components/PointCounter/PointCounter.tsx
+++ src/components/PointCounter/PointCounter.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import PointCounterClass from '../../classes/PointCounter'
+import Firework from '../Firework/Firework'
+import Modal from '../Modal/Modal'
 import ProgressBar from '../ProgressBar/ProgressBar'
 import styles from './PointCounter.module.css'
 
@@ -11,11 +13,29 @@ function PointCounter({ pointCounter }: Props): JSX.Element {
   const finishedPoints = pointCounter.getFinishedPoints()
   const maxPoints = pointCounter.getMaxPoints()
   const [percentage, setPercentage] = useState(0)
+  // const [hasCelebrated, setHasCelebrated] = useState(false)
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleCloseModal = () => {
+    setIsOpen(!isOpen)
+  }
 
   useEffect(() => {
     const percent = Math.round((finishedPoints / maxPoints) * 100)
     setPercentage(percent)
   }, [finishedPoints, maxPoints])
+
+  useEffect(() => {
+    if (percentage === 100) {
+      // setHasCelebrated(true)
+      handleCloseModal()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [percentage])
+
+  // useEffect(() => {
+  //   setHasCelebrated(false)
+  // }, [maxPoints])
 
   return (
     <div className={styles.pointCounterContainer}>
@@ -29,6 +49,13 @@ function PointCounter({ pointCounter }: Props): JSX.Element {
             labelColor="#fff9f2"
           />
         </div>
+      )}
+      {isOpen && (
+        <Modal handleCloseModal={handleCloseModal}>
+          <h1>Hooray!</h1>
+          <p>You finished all your tasks. Well done!</p>
+          <Firework />
+        </Modal>
       )}
     </div>
   )
