7b012c213c65d94ce9a820c3c5fcc4354a2fcc38
Rebecca Axelsson <ra223ai@student.lnu.se>
2022-01-20 09:23:04 +0100

    💄  Add svg wave to header

--- src/components/ToDo/ToDoComp.tsx
+++ src/components/ToDo/ToDoComp.tsx
@@ -91,11 +91,14 @@ function ToDoComp({ todo } : Props): JSX.Element {
       <h1>Fun To Do</h1>
         <PointCounter pointCounter={pointCounterClass}/>
       </div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path fill="#2a9d8f" fillOpacity="1" d="M0,160L80,133.3C160,107,320,53,480,69.3C640,85,800,171,960,176C1120,181,1280,107,1360,69.3L1440,32L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z" />
+        </svg>
       <div className={styles.taskListWrapper}>
         <UnfinishedTasks unfinishedTaskList={unfinishedTasks} handleToggleStatus={handleToggleStatus} hasFinishedTasks={hasFinishedTasks()} handleDeleteTask={handleDeleteTask} />
         {(!finishedTasks || finishedTasks.length < 1) ? null : <FinishedTasks finishedTaskList={finishedTasks} handleToggleStatus={handleToggleStatus} handleDeleteTask={handleDeleteTask} /> }
       </div>
-      <div className={styles.addTaskForm} >
+      <div className={styles.addTaskForm}>
         <form onSubmit={e => handleNewTask(e)}>
           <div className={styles.formInputs}>
             <input
