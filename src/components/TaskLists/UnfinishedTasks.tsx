eac136114cd3424c360e6fd1b04a46936e5d2ae8
Rebecca Axelsson <ra223ai@student.lnu.se>
2022-01-21 08:41:54 +0100

    ⚡️  Animate list items

--- src/components/TaskLists/UnfinishedTasks.tsx
+++ src/components/TaskLists/UnfinishedTasks.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 import Task from '../Task/Task'
 import TaskClass from '../../classes/Task'
 
@@ -7,6 +8,33 @@ type Props = {
   handleToggleStatus: any
   hasFinishedTasks: boolean
   handleDeleteTask: any
+}
+
+const list = {
+  visible: {
+    opacity: 1,
+    transition: {
+      type: 'spring',
+      duration: 2,
+      staggerChildren: 0.5
+    }
+  },
+  hidden: {
+    opacity: 0
+  }
+}
+
+const item = {
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: 'spring',
+      duration: 0.7,
+      bounce: 0.4
+    }
+  },
+  hidden: { opacity: 0, x: -100 }
 }
 
 function UnfinishedTasks({
@@ -20,17 +48,17 @@ function UnfinishedTasks({
       {!unfinishedTaskList || (unfinishedTaskList.length === 0 && !hasFinishedTasks) ? (
         <p className="noTasks">No tasks</p>
       ) : (
-        <ul>
+        <motion.ul initial="hidden" animate="visible" variants={list}>
           {unfinishedTaskList.map((task: TaskClass) => (
-            <li>
+            <motion.li variants={item}>
               <Task
                 task={task}
                 handleToggleStatus={handleToggleStatus}
                 handleDeleteTask={handleDeleteTask}
               />
-            </li>
+            </motion.li>
           ))}
-        </ul>
+        </motion.ul>
       )}
     </>
   )
